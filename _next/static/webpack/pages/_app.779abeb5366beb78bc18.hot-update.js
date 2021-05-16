7a4b41f8bcc7bb790c104fe81af529b544035aac
Bunlong <bunlong.van@gmail.com>
2021-05-16 22:18:03 +0700

    :rocket: v0.1.0

---
+++ _next/static/webpack/pages/_app.779abeb5366beb78bc18.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/_app",{
+
+/***/ "./sidebar.json":
+/*!**********************!*\
+  !*** ./sidebar.json ***!
+  \**********************/
+/*! exports provided: path, name, children, size, type, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"path\\\":\\\"pages\\\",\\\"name\\\":\\\"pages\\\",\\\"children\\\":[{\\\"path\\\":\\\"pages/index.mdx\\\",\\\"name\\\":\\\"index.mdx\\\",\\\"meta\\\":{},\\\"title\\\":\\\"Algorithms Hub\\\",\\\"url\\\":\\\"/\\\"},{\\\"path\\\":\\\"pages/maths\\\",\\\"name\\\":\\\"maths\\\",\\\"children\\\":[{\\\"path\\\":\\\"pages/maths/absolute.mdx\\\",\\\"name\\\":\\\"absolute.mdx\\\",\\\"meta\\\":{},\\\"title\\\":\\\"Absolute\\\",\\\"url\\\":\\\"/maths/absolute\\\"},{\\\"path\\\":\\\"pages/maths/aliquotsum.mdx\\\",\\\"name\\\":\\\"aliquotsum.mdx\\\",\\\"meta\\\":{},\\\"title\\\":\\\"Aliquotsum\\\",\\\"url\\\":\\\"/maths/aliquotsum\\\"}],\\\"size\\\":null,\\\"type\\\":\\\"directory\\\"}],\\\"size\\\":null,\\\"type\\\":\\\"directory\\\"}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3NpZGViYXIuanNvbi5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./sidebar.json\n");
+
+/***/ })
+
+})
